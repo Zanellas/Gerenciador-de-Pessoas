abe20b762dd19759ec2b6a162697f4570590f93f
Joao Pedro Zanella Kirst <95651866+Zanellas@users.noreply.github.com>
2023-02-09 12:15:36 -0400

    Update AddressEntity.java

--- src/main/java/com/api/gerenciarpessoas/entities/AddressEntity.java
+++ src/main/java/com/api/gerenciarpessoas/entities/AddressEntity.java
@@ -30,7 +30,7 @@ public class AddressEntity implements Serializable {
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "ID_PERSON", foreignKey = @ForeignKey(name = "FK_ADDRESS_PERSON"))
 	@JsonBackReference
-	private Person person;
+	private PersonEntity person;
 
 	@Column(name = "STREET", nullable = false, length = 150)
 	private String street;
@@ -45,6 +45,79 @@ public class AddressEntity implements Serializable {
 	private String city;
 
 	@Column(name = "MAIN", nullable = false)
-	private Boolean main;	
+	private Boolean main;
 
+	public AddressEntity(Long id, PersonEntity person, String street, String cep, String number, String city,
+			Boolean main) {
+		this.id = id;
+		this.person = person;
+		this.street = street;
+		this.cep = cep;
+		this.number = number;
+		this.city = city;
+		this.main = main;
+	}
+	
+	public AddressEntity() {
+
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public PersonEntity getPerson() {
+		return person;
+	}
+
+	public void setPerson(PersonEntity person) {
+		this.person = person;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Boolean getMain() {
+		return main;
+	}
+
+	public void setMain(Boolean main) {
+		this.main = main;
+	}	
+
+	
 }
